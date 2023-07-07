9f0f2f656464d0337b1f3e7693a138f04f8450dd
Pedro Martins <pedroms410@gmail.com>
2023-07-07 08:37:20 -0300

    fix viewBox for qgis 3.30

--- DSGTools_Op/ProfileTool/pyqtgraph/graphicsItems/ViewBox/ViewBox.py
+++ DSGTools_Op/ProfileTool/pyqtgraph/graphicsItems/ViewBox/ViewBox.py
@@ -1198,7 +1198,7 @@ class ViewBox(GraphicsWidget):
         #return scale
 
     def wheelEvent(self, ev, axis=None):
-        mask = np.array(self.state['mouseEnabled'], dtype=np.float)
+        mask = np.array(self.state['mouseEnabled'], dtype=float)
         if axis is not None and axis >= 0 and axis < len(mask):
             mv = mask[axis]
             mask[:] = 0
@@ -1240,7 +1240,7 @@ class ViewBox(GraphicsWidget):
         dif = dif * -1
 
         ## Ignore axes if mouse is disabled
-        mouseEnabled = np.array(self.state['mouseEnabled'], dtype=np.float)
+        mouseEnabled = np.array(self.state['mouseEnabled'], dtype=float)
         mask = mouseEnabled.copy()
         if axis is not None:
             mask[1-axis] = 0.0
