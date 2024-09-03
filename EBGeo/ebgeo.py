7ed0922d179a96a0753828d285014fe2217416ae
Pedro Martins <pedroms410@gmail.com>
2024-09-03 14:04:20 -0300

    retirar perfil do terreno (usar nativo)

--- EBGeo/ebgeo.py
+++ EBGeo/ebgeo.py
@@ -155,7 +155,7 @@ class EBGeo(QObject):
 
 		self.azgen_action = self.add_action(
 			os.path.join(os.path.dirname(__file__), 'icons', 'azimuthgen.png'),
-			text=u'Gerador de azimute e distância',
+			text=u'Calcula azimute e distância (tabela)',
 			callback=self.loadAzimuthGenerator,
 			parent=self.ebGeo,
 			add_to_menu=False,
@@ -176,16 +176,16 @@ class EBGeo(QObject):
 		from .AreaRange.areaRange import AreaRange as Main_AreaRange
 		self.mainAreaRange = Main_AreaRange(iface)
 
-		self.pt_action = self.add_action(
-			os.path.join(os.path.dirname(__file__), 'icons', 'profileIcon.png'),
-			text=u'Gerador de perfil do terreno',
-			callback=self.loadProfileTool,
-			parent=self.ebGeo,
-			add_to_menu=False,
-			add_to_toolbar=False)
-		self.ebGeo.addAction(self.pt_action)
-		from .ProfileTool.profileplugin import ProfilePlugin as Main_ProfileTool
-		self.mainProfileTool = Main_ProfileTool(iface)
+		# self.pt_action = self.add_action(
+		# 	os.path.join(os.path.dirname(__file__), 'icons', 'profileIcon.png'),
+		# 	text=u'Gerador de perfil do terreno',
+		# 	callback=self.loadProfileTool,
+		# 	parent=self.ebGeo,
+		# 	add_to_menu=False,
+		# 	add_to_toolbar=False)
+		# self.ebGeo.addAction(self.pt_action)
+		# from .ProfileTool.profileplugin import ProfilePlugin as Main_ProfileTool
+		# self.mainProfileTool = Main_ProfileTool(iface)
 
 		self.vis_action = self.add_action(
 			os.path.join(os.path.dirname(__file__), 'icons', 'visib.png'),
